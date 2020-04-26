ab9413b55bdd180beb3baed513bfedca49e125dd
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-04-26 15:51:40 -0300

    Set base URL for axios

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,16 @@
 const mix = require('laravel-mix');
+const webpack = require('webpack');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.webpackConfig({
+    plugins: [
+        new webpack.DefinePlugin({
+            env: {
+                BASE_URL: JSON.stringify(process.env.APP_URL)
+            }
+        })
+    ]
+});
+
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
