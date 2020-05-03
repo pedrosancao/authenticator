ba0689884860be3b30faef192431a1ec1bd24b13
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-05-03 19:15:56 -0300

    Add click to copy code

--- resources/js/components/accounts.vue
+++ resources/js/components/accounts.vue
@@ -5,11 +5,12 @@
                 <div class="progress-bar progress-bar-striped progress-bar-animated bg-secondary w-100" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
         </div>
-        <div v-for="(account, index) in accounts" v-bind:key="index" class="col-12 col-md-3">
-            <div class="card">
+        <div v-for="(account, index) in accounts" :key="index" class="col-12 col-md-3">
+            <div class="card" @click="copyCode(index)" :ref="'container-' + index">
                 <div class="card-header">{{ account.name }}</div>
                 <div class="card-body d-flex justify-content-between">
                     <span>{{ account.code.match(/.{1,3}/g).join(' ') }}</span>
+                    <input class="position-fixed" type="text" :value="account.code" :ref="'input-' + index">
                     <clock :radius="10"></clock>
                 </div>
             </div>
@@ -24,12 +25,12 @@
             accounts: []
         }),
         mounted() {
-            this.getaccounts().then(() => {
-                setInterval(this.getaccounts, 30000)
+            this.getAccounts().then(() => {
+                setInterval(this.getAccounts, 30000)
             })
         },
         methods: {
-            getaccounts: function() {
+            getAccounts() {
                 this.loading = true
                 return axios.get('/codes').then(response => {
                     this.loading = false
@@ -38,7 +39,28 @@
                         this.accounts = response.data
                     })
                 })
+            },
+            copyCode(index) {
+                let container = this.$refs['container-' + index][0]
+                let input = this.$refs['input-' + index][0]
+                let highlightClass = 'border-info'
+                input.select()
+                document.execCommand('copy')
+                input.blur()
+                container.classList.add(highlightClass)
+                setTimeout(() => {
+                    container.classList.remove(highlightClass)
+                }, 1500);
             }
         }
     }
 </script>
+
+<style scoped>
+.card {
+    transition: border 1s ease;
+}
+input {
+    z-index: -1;
+}
+</style>
